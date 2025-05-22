c7658380d9d78f687fd36bad3cec07d4eb615037
Rohiphop <rodrigoj.jara.mendez@gmail.com>
2025-05-22 03:59:27 -0400

    UsuarioController
    Creado

--- FitLife Microservicios/Usuarios/src/main/java/com/example/Usuarios/controller/UsuariosController.java
+++ FitLife Microservicios/Usuarios/src/main/java/com/example/Usuarios/controller/UsuariosController.java
@@ -3,9 +3,12 @@ package com.example.Usuarios.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Usuarios.Model.Rol;
@@ -42,4 +45,15 @@ public class UsuariosController {
     }
 
     //ENDPOINT PARA CREAR USUARIO NUEVO
+    @PostMapping("/users")
+    public ResponseEntity<?> crearUsuario(@RequestParam String username, @RequestParam String Password, @RequestParam Long rolId){
+        try{
+            //intento de crear usuario
+            Usuarios nuevo =  usuariosService.crearUsuario(username, Password, rolId);
+            return ResponseEntity.status(HttpStatus.CREATED).body(nuevo);
+        }   catch (RuntimeException e){
+            //si no crea el usuario, salta string del erro
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
 }
